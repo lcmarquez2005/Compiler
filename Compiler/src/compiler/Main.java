b18f0363a604dd87a0101619c0f0452f1ed05f04
A-Common-Person <acommonpersonofficial@gmail.com>
2025-07-14 20:02:08 -0600

    CorreccionMain

--- Compiler/src/compiler/Main.java
+++ Compiler/src/compiler/Main.java
@@ -3,30 +3,26 @@ package compiler;
 import java.util.Scanner;
 
 public class Main {
-public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
-Lexer lexer = new Lexer();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Lexer lexer = new Lexer();
 
-pgsql
-Copiar
-Editar
+        System.out.println("Analizador léxico básico");
+        System.out.println("Escribe una expresión y presiona Enter. Escribe 'salir' para terminar.");
 
-    System.out.println("Analizador léxico básico");
-    System.out.println("Escribe una expresión y presiona Enter. Escribe 'salir' para terminar.");
+        while (true) {
+            System.out.print("> ");
+            String expresion = scanner.nextLine();
 
-    while (true) {
-        System.out.print("> ");
-        String expresion = scanner.nextLine();
+            if (expresion.equalsIgnoreCase("salir")) {
+                System.out.println("Programa finalizado.");
+                break;
+            }
 
-        if (expresion.equalsIgnoreCase("salir")) {
-            System.out.println("Programa finalizado.");
-            break;
+            lexer.analizar(expresion);
+            System.out.println(); // Espacio entre expresiones
         }
 
-        lexer.analizar(expresion);
-        System.out.println(); // Espacio entre expresiones
+        scanner.close();
     }
-
-    scanner.close();
-}
 }
