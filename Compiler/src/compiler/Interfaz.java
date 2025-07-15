2156cf4cf9109cafc4da729581fb2d9fdae9f16e
lcmarquez2005 <93497992+lcmarquez2005@users.noreply.github.com>
2025-07-14 21:15:19 -0600

    Corregido lo de alisson: ya se muestra la ejecucion de la interfaz en simultaneo del modo cli.

--- Compiler/src/compiler/Interfaz.java
+++ Compiler/src/compiler/Interfaz.java
@@ -166,7 +166,6 @@ public class Interfaz extends JFrame {
     }
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -179,15 +178,8 @@ public class Interfaz extends JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 300, Short.MAX_VALUE)
         );
-
         pack();
     }// </editor-fold>//GEN-END:initComponents
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            Interfaz gui = new Interfaz();
-            gui.setVisible(true);
-        });
-    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 }
--- Compiler/src/compiler/Main.java
+++ Compiler/src/compiler/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Lexer lexer = new Lexer();
-
+            Interfaz gui = new Interfaz();
+            gui.setVisible(true);
         System.out.println("Analizador léxico básico");
         System.out.println("Escribe una expresión y presiona Enter. Escribe 'salir' para terminar.");
 
